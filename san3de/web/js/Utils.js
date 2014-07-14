a85fa4c137557e6b38ece47a3bb7643138784b20
Kram <kram@acrecer.com>
2014-07-14 17:29:22 -0500

    Documentation of the code

--- san3de/web/js/Utils.js
+++ san3de/web/js/Utils.js
@@ -1,9 +1,19 @@
+/*===================================================
+	Here are some common functions that are used 
+	along all the game
+===================================================*/
 var Utils = {
-	get: function(objectId){
+	/*===================================================
+		Returns an HTML element by its ID
+	===================================================*/
+	get: function(/*String*/ objectId){
 		return document.getElementById(objectId);
 	},
 	
-	addEvent: function(obj, type, func){
+	/*===================================================
+		Adds a event listener regarding the browser
+	===================================================*/
+	addEvent: function(/*Element*/ obj, /*String*/ type, /*Function*/ func){
 		if (obj.attachEvent){
 			obj.attachEvent("on" + type, func);
 		}else if (obj.addEventListener){
@@ -11,6 +21,10 @@ var Utils = {
 		}
 	},
 	
+	/*===================================================
+		Returns a XMLHttpRequest object no matter
+		the browser
+	===================================================*/
 	getHttp: function(){
 		var http;
 		if  (window.XMLHttpRequest){
@@ -23,7 +37,11 @@ var Utils = {
 	}
 };
 
-Math.iRandom = function(a, b){
+/*===================================================
+	Returns a random integer number between 1 or
+	2 parameters
+===================================================*/
+Math.iRandom = function(/*Int*/ a, /*Int*/ b){
 	if (b == undefined){
 		b = a;
 		a = 0;
@@ -33,12 +51,16 @@ Math.iRandom = function(a, b){
 	return a + Math.floor(Math.random() * b);
 };
 
-Math.getAngle = function(/*vec2*/ a, /*vec2*/ b){
+/*===================================================
+	Gets the angle between 2 points 
+===================================================*/
+Math.getAngle = function(/*Vec2*/ a, /*Vec2*/ b){
 	var xx = Math.abs(a.a - b.a);
 	var yy = Math.abs(a.b - b.b);
 	
 	var ang = Math.atan2(yy, xx);
 	
+	// Adjust the angle according to both positions
 	if (b.a <= a.a && b.b <= a.b){
 		ang = Math.PI - ang;
 	}else if (b.a <= a.a && b.b > a.b){
@@ -52,7 +74,10 @@ Math.getAngle = function(/*vec2*/ a, /*vec2*/ b){
 	return ang;
 };
 
-Math.getShortAngle = function(a1, a2){
+/*===================================================
+	Gets the minimum angle between 2 angles
+===================================================*/
+Math.getShortAngle = function(/*float*/ a1, /*float*/ a2){
     var angle = (Math.abs(a1 - a2)) % Math.PI2;
 
     if(angle > Math.PI)
@@ -61,20 +86,34 @@ Math.getShortAngle = function(a1, a2){
     return angle;
 };
 
+/*===================================================
+	Gets the distance between two points using
+	the pythagoras theorem
+===================================================*/
 Math.getDistance = function(/*Vec2*/ a, /*Vec2*/ b){
 	return Math.sqrt(Math.pow(a.a - b.a, 2) + Math.pow(a.b - b.b, 2));
 };
 
+// Common angles 90, 270, 360 in radians
 Math.PI2 = Math.PI * 2;
 Math.PI_2 = Math.PI / 2;
 Math.PI3_2 = 3 * Math.PI / 2;
-Math.degToRad = function(value){
+
+/*===================================================
+	Transforms a degree angle to a radian
+===================================================*/
+Math.degToRad = function(/*float*/ value){
 	return value * Math.PI / 180;
 };
-Math.radToDeg = function(value){
+
+/*===================================================
+	Transforms a radian angle to a degree
+===================================================*/
+Math.radToDeg = function(/*float*/ value){
 	return value * 180 / Math.PI;
 };
 
+// Depending on the browser, this function can be different
 window.requestAnimFrame = 
 	window.requestAnimationFrame       || 
 	window.webkitRequestAnimationFrame || 
@@ -82,5 +121,5 @@ window.requestAnimFrame =
 	window.oRequestAnimationFrame      || 
 	window.msRequestAnimationFrame     || 
 	function(/* function */ draw1){
-		window.setTimeout(draw1, 1000 / 60);
+		window.setTimeout(draw1, 1000 / 30);
 	};
